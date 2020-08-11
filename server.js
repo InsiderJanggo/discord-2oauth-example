52693ce7a23225c325aae7f34391879c37f326ab
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-08-11 11:43:53 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+const fetch = require('node-fetch');
+
+const {CLIENT_ID, CLIENT_SECRET, REDIRECT_LINK, AUTH_CODE, SCOPE} = require('./config.js');
 const port = 53134;
 
 http.createServer((req, res) => {
 	let responseCode = 404;
 	let content = '404 Error';
 
-	if (req.url === '/') {
+	const urlObj = url.parse(req.url, true);
+
+	if (urlObj.query.code) {
+		const accessCode = urlObj.query.code;
+		const data = {
+			client_id: CLIENT_ID,
+			client_secret: CLIENT_SECRET,
+			grant_type: AUTH_CODE,
+			redirect_uri: REDIRECT_LINK,
+			code: accessCode,
+			scope: SCOPE,
+		};
+		console.log(`The Code is ${accessCode}`)
+		fetch('https://discordapp.com/api/oauth2/token', {
+			method: 'POST',
+			body: new URLSearchParams(data),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		})
+			.then(discordRes => discordRes.json())
+			.then(info => {
+				console.log(info);
+				return info;
+			})
+			.then(info => fetch('https://discordapp.com/api/users/@me', {
+				headers: {
+					authorization: `${info.token_type} ${info.access_token}`,
+				},
+			}))
+			.then(userRes => userRes.json())
+			.then(console.log);
+	}
+
+	if (urlObj.pathname === '/') {
 		responseCode = 200;
 		content = fs.readFileSync('./views/index.html');
 	}
@@ -19,3 +57,5 @@ http.createServer((req, res) => {
 	res.end();
 })
 	.listen(port);
+
+console.log(`Listening To ${port}`);
